d84b29cefe7cf97f0264494794acd2c8f74f0a36
teerawat prarom <teewat@yahoo.com>
2021-11-07 22:47:10 +1100

    updated completed assignment

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,8 +16,11 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
- arr = [DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G]
-  arr
+  [ {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
+    {:name => "Pat Monteseian", :occupation => "Staff"},
+    {:name => "Kate Gompert", :occupation => "None"},
+    {:name => "Bruce Green", :occupation => "Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
